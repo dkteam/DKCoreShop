ef3d787bcb218927d8ec6e424b46357c77207f43
Pham Dang Khoa <mr.pdkhoa@gmail.com>
2018-11-07 08:56:43 +0700

    Lesson19 Login with identity

---
+++ DKCoreShop/wwwroot/app/controllers/login/index.js
@@ -0,0 +1,35 @@
+﻿var loginController = function () {
+    this.initialize = function () {
+        registerEvents();
+    }
+
+    var registerEvents = function () {
+        $('#btnLogin').on('click',  function (e) {
+            e.preventDefault();
+            var user = $('#txtUserName').val();
+            var password = $('#txtPassword').val();
+            login(user, password);
+        });
+
+    }
+
+    var login = function (user, pass) {
+        $.ajax({
+            type: 'POST',
+            data: {
+                UserName: user,
+                Password: pass
+            },
+            dateType: 'json',
+            url: "/admin/login/authen",
+            success: function (res) {
+                if (res.Success) {
+                    window.location.href = "/Admin/Home/index";
+                }
+                else {
+                    dkteam.notify('Thông tin đăng nhập không đúng', 'error');
+                }
+            }
+        })
+    }
+}
