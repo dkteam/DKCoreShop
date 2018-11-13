ec0f924d151b869762cfc8cc6005f36d10d66607
Pham Dang Khoa <mr.pdkhoa@gmail.com>
2018-11-13 09:35:00 +0700

    Display product category with jquery-easyui tree view

---
+++ DKCoreShop/Areas/Admin/Controllers/ProductCategoryController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DKCoreShop.Application.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DKCoreShop.Areas.Admin.Controllers
+{
+    public class ProductCategoryController : BaseController
+    {
+        IProductCategoryService _productCategoryService;
+
+        public ProductCategoryController(IProductCategoryService productCategoryService)
+        {
+            _productCategoryService = productCategoryService;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        #region Get Data API
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var model = _productCategoryService.GetAll();
+            return new OkObjectResult(model);
+        }
+        #endregion
+    }
+}
---
+++ DKCoreShop/wwwroot/app/controllers/product-category/index.js
@@ -0,0 +1,30 @@
+﻿var productCategoryController = function () {
+    this.initialize = function () {
+        loadData();
+    }
+
+    function loadData() {
+        $.ajax({
+            url: '/Admin/ProductCategory/GetAll',
+            dataType: 'json',
+            success: function (response) {
+                var data = [];
+                $.each(response, function (i, item) {
+                    data.push({
+                        id: item.Id,
+                        text: item.Name,
+                        parentId: item.ParentId,
+                        sortOrder: item.SortOrder
+                    });
+                    var treeArr = dkteam.unflattern(data);
+
+                    //var $tree = $('#treeProductCategory');
+
+                    $('#treeProductCategory').tree({
+                        data: treeArr
+                    });
+                });
+            }
+        });
+    }
+}
